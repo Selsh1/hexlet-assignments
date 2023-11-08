01b1c25443a5485659d8a07da6be7c4941092e75
yudinna <yudinna@nspk.ru>
2023-11-08 17:03:22 +0300

    add buildApartmentsList

--- java-oop-ru/interfaces/src/main/java/exercise/App.java
+++ java-oop-ru/interfaces/src/main/java/exercise/App.java
@@ -1,8 +1,16 @@
 package exercise;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 // BEGIN
-
+public class App {
+    public static List<String> buildApartmentsList(List<Home> homes, int n) {
+        return homes
+                .stream()
+                .sorted(Home::compareTo)
+                .limit(n)
+                .map(Object::toString)
+                .toList();
+    }
+}
 // END
