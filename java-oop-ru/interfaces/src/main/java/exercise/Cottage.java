052e3cf7aa2d51b4dd5752bdb8a39b1f5ad288f9
yudinna <yudinna@nspk.ru>
2023-11-08 16:30:45 +0300

    add cottage

--- java-oop-ru/interfaces/src/main/java/exercise/Cottage.java
+++ java-oop-ru/interfaces/src/main/java/exercise/Cottage.java
@@ -1,5 +1,29 @@
 package exercise;
 
 // BEGIN
+public class Cottage implements Home {
+    private double area;
+    private int floorCount;
 
+    public Cottage(double area, int floorCount) {
+        this.area = area;
+        this.floorCount = floorCount;
+    }
+
+
+    @Override
+    public double getArea() {
+        return this.area;
+    }
+
+    @Override
+    public double compareTo(Home another) {
+        return this.getArea() - another.getArea();
+    }
+
+    @Override
+    public String toString() {
+        return this.floorCount + " этажный коттедж площадью " + this.getArea() + " метров";
+    }
+}
 // END
