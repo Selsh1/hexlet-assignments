2ac800bf78b4c821304e90cf08408304137c614b
yudinna <yudinna@nspk.ru>
2023-11-08 15:53:25 +0300

    add segment

--- java-oop-ru/classes-and-objects/src/main/java/exercise/Segment.java
+++ java-oop-ru/classes-and-objects/src/main/java/exercise/Segment.java
@@ -1,5 +1,32 @@
 package exercise;
 
 // BEGIN
+public class Segment {
+    private Point begin;
+    private Point end;
 
+    public Segment(Point begin, Point end) {
+        this.begin = begin;
+        this.end = end;
+    }
+
+    public Point getBeginPoint() {
+        return this.begin;
+    }
+
+    public Point getEndPoint() {
+        return this.end;
+    }
+
+    public Point getMidPoint() {
+        int x = Segment.mid(this.begin.getX(), this.end.getX());
+        int y = Segment.mid(this.begin.getX(), this.end.getY());
+
+        return new Point(x, y);
+    }
+
+    private static int mid(int begin, int end) {
+        return (begin + end) / 2;
+    }
+}
 // END
